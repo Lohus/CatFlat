05fed8bfa7ca6576b5e38f24e40b231fa06ec89b
Lohus <alhov271@gmail.com>
2025-12-15 23:28:32 +0500

    Respawn cat

--- UI/MenuPanel.cs
+++ UI/MenuPanel.cs
@@ -1,13 +1,15 @@
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MenuPanel : MonoBehaviour
 {
-    [SerializeField] Button replay, home;
+    [SerializeField] Button replay, home, rewarded;
     void Start()
     {
-        replay.onClick.AddListener(() => LoadScene("GameScene"));
+        //replay.onClick.AddListener(() => LoadScene("GameScene"));
+        replay.onClick.AddListener(() => RewardedButton());
         home.onClick.AddListener(() => LoadScene("MainMenu"));
 
         void LoadScene(string nameScene)
@@ -19,5 +21,22 @@ public class MenuPanel : MonoBehaviour
             }
             SceneManager.LoadScene(nameScene);
         }
+    }
+
+    void RewardedButton()
+    {
+        GameObject[] platforms =  GameObject.FindGameObjectsWithTag("Platform");
+        GameObject lowestPlatform = null;
+        float lowestY = Mathf.Infinity;
+        foreach (GameObject platform in platforms)
+        {
+            float currentY = platform.transform.position.y;
+            if (currentY < lowestY)
+            {
+                lowestY = currentY;
+                lowestPlatform = platform;
+            }
+        }
+        Player.instance.transform.position = lowestPlatform.transform.position + new UnityEngine.Vector3(0, 1, 0);
     }
 }
